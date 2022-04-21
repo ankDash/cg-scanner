6f112005852609dbc4a89582d563a7cf208e7077
ankDash <104131592+ankDash@users.noreply.github.com>
2022-04-21 14:26:11 +0530

    Create FileValidation.js

---
+++ utility/FileValidation.js
@@ -0,0 +1,14 @@
+exports.fileValidation = async (filename,extension) => {  
+    try{
+        let validExtension = false;
+        if(filename.split('.')[1].toString().trim().toLowerCase() === extension){
+            validExtension = true;
+        } else{
+            validExtension = false;
+        }
+        return validExtension;
+     
+    }catch(err){
+        return false;
+    }
+}
